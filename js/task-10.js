d80c9c1c68bfcdba1f4ed14eb3c02e52023b03b5
Ilya Bondarchuk <cod3provider@gmail.com>
2022-05-14 15:34:05 +0300

    all 10 tasks done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,37 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const inputCreateBox = document.querySelector('#controls > input');
+const boxesDiv = document.querySelector('#boxes');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+let initialRatioBox = 30;
+
+createBtn.addEventListener('click', createAmountBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createAmountBoxes() {
+    const amountOfBoxes = createBoxes(inputCreateBox.value);
+    boxesDiv.append(...amountOfBoxes);
+}
+
+function createBoxes(amount) {
+    let ratioBox = 0;
+    const boxElements = [];
+    for (let i = 0; i < amount; i += 1) {
+        const newDiv = document.createElement('div');
+        newDiv.style.backgroundColor = getRandomHexColor();
+        ratioBox = initialRatioBox + i * 10;
+        newDiv.style.width = `${ratioBox}px`;
+        newDiv.style.height = `${ratioBox}px`;
+        boxElements.push(newDiv);
+    }
+    initialRatioBox = ratioBox + 10;
+    return boxElements;
+}
+
+function destroyBoxes() {
+    boxesDiv.innerHTML = '';
+    initialRatioBox = 30;
 }
