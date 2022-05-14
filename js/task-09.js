fac2560388a9ee50a4ffc6a02864d14cf497df86
Ilya Bondarchuk <cod3provider@gmail.com>
2022-05-14 13:30:46 +0300

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeBtnColor = document.querySelector('.change-color');
+const outputColor = document.querySelector('.color');
+
+changeBtnColor.addEventListener('click', event => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  outputColor.textContent = document.body.style.backgroundColor;
+});
+
+
