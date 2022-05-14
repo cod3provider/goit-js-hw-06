4ba04028e6e142910e03cffcdfe3ad849f9d8ad3
Ilya Bondarchuk <cod3provider@gmail.com>
2022-05-14 10:44:01 +0300

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', (event) => {
+	event.preventDefault();
+
+	const elementsForm = event.currentTarget.elements;
+
+	const mail = elementsForm.email.value;
+	const password = elementsForm.password.value;
+
+	const formDataValue = {mail, password};
+
+	if (mail === "" || password === "") {
+		alert('Не все поля заполнены!');
+	} else {
+		console.log(formDataValue);
+	}
+
+	event.currentTarget.reset();
+});
