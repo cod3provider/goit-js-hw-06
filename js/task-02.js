77f31318ac609a9923d08a8247f9ff7349ef5b0b
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-08-21 18:07:03 +0300

    task 02 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,30 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const createIngredientEl = function (elements) {
+  const createEl = elements.map(element => {
+    const itemEl = document.createElement('li');
+    itemEl.textContent = `${element}`;
+    itemEl.classList.add('item');
+
+    return itemEl;
+  });
+
+  const ingredientsListEl = document.querySelector('#ingredients');
+  ingredientsListEl.append(...createEl);
+  return ingredientsListEl;
+}
+
+createIngredientEl(ingredients);
+
+
+
+
+
+
+
+
+// console.log(createIngredientEl(ingredients))
+
