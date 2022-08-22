d7232cd300799f98009e1e8d25c5acbffafab138
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-08-22 17:47:48 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,6 @@
 const inputEl = document.querySelector('#validation-input');
 
 const checkInputLength = evt => {
-	console.log(evt.currentTarget);
-	console.log(evt.currentTarget.value.length);
-
 	if (evt.currentTarget.value.length !== Number(inputEl.dataset.length)) {
 		evt.currentTarget.classList.remove('valid');
 		evt.currentTarget.classList.add('invalid');
