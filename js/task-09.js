7dc136e8bf759812c60e6d4a7701142d931439f7
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-08-23 14:19:51 +0300

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const bodyEl = document.querySelector('body');
+const colorSpanEl = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', onChangeBtnColorClick);
+
+function onChangeBtnColorClick() {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorSpanEl.textContent = bodyEl.style.backgroundColor;
+}
