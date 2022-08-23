8b2171c5821833268b2ee5e7864f362a55176433
Ilya Bondarchuk <89864785+cod3provider@users.noreply.github.com>
2022-08-23 13:59:42 +0300

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmitHandler)
+
+function onFormSubmitHandler(evt) {
+	evt.preventDefault();
+
+	const email = evt.currentTarget.elements.email.value;
+	const password = evt.currentTarget.elements.password.value;
+
+	if (!email || !password) {
+		alert('Ты не прав. Не все поля заполнены. Подумай еще!');
+	}
+
+	const formDataValue = {email, password};
+	console.log(formDataValue);
+	evt.currentTarget.reset();
+}
